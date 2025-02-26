85cd88a9bc09bf35766637b4822add92ed8bc622
kaiyamashita08 <114874647+kaiyamashita08@users.noreply.github.com>
2025-02-25 20:13:47 -0800

    maybe

--- vite.config.js
+++ vite.config.js
@@ -38,13 +38,13 @@ export default defineConfig({
 				orientation: "portrait",
 				icons: [
 					{
-						"src": "/pwa-512x512.png",
+						"src": "pwa-512x512.png",
 						"sizes": "512x512",
 						"type": "image/png",
 						"purpose": "maskable"
 					},
 					{
-						"src": "/pwa-192x192.png",
+						"src": "pwa-192x192.png",
 						"sizes": "192x192",
 						"type": "image/png",
 						"purpose": "any"
