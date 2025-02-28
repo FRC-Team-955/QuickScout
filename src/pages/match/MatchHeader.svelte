8e22075397f73d6615f029fa343e0189df88b4dd
amsam0 <44983869+amsam0@users.noreply.github.com>
2025-02-27 19:17:09 -0800

    Add disconnected alert

--- src/pages/match/MatchHeader.svelte
+++ src/pages/match/MatchHeader.svelte
@@ -1,5 +1,6 @@
 <script>
 	import { scouterState } from "../../queuing/scouter.svelte";
+	import { serverState } from "../../queuing/shared.svelte";
 	import { matchData } from "../../state.svelte";
 </script>
 
@@ -8,5 +9,11 @@
 {#if scouterState.isInOnlineMatch && scouterState.matchEndSignaled}
 	<h3 style="color: orange">
 		Scouting lead has signaled that the match is ended. Please finalize and upload your data.
+	</h3>
+{/if}
+
+{#if scouterState.isInOnlineMatch && !serverState.connected}
+	<h3 style="color: orange">
+		You are not connected to the server. You can continue scouting but you must export via QR code.
 	</h3>
 {/if}
