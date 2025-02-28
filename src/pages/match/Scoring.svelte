a3d82ae9365fb1a1e232008b810208372e1d4505
amsam0 <44983869+amsam0@users.noreply.github.com>
2025-02-27 19:47:33 -0800

    Undo and other scoring improvements

--- src/pages/match/Scoring.svelte
+++ src/pages/match/Scoring.svelte
@@ -1,80 +1,184 @@
 <script>
+	import { Check } from "lucide-svelte";
 	import Button from "../../components/Button.svelte";
 	import { matchData, scoringState } from "../../state.svelte";
 
 	let { isAutonomous } = $props();
 
+	/** @type {(() => void)[]} */
+	let actionUndos = $state([]);
+	let undoIcon = $state(null);
+	let undoCooldown = $state(false);
+
+	function undoLastAction() {
+		if (!undoCooldown && actionUndos.length > 0) {
+			actionUndos.pop()();
+			undoIcon = Check;
+			undoCooldown = true;
+			setTimeout(() => {
+				undoIcon = null;
+				undoCooldown = false;
+			}, 500);
+		}
+	}
+
 	function coralPreload() {
 		(isAutonomous ? matchData.autonomous : matchData.teleop).coral.preloaded = true;
 		scoringState.hasCoral = true;
+		actionUndos.push(() => {
+			(isAutonomous ? matchData.autonomous : matchData.teleop).coral.preloaded = false;
+			scoringState.hasCoral = false;
+		});
 	}
 	function coralStationPickup() {
 		(isAutonomous ? matchData.autonomous : matchData.teleop).coral.stationPickup++;
 		scoringState.hasCoral = true;
+		actionUndos.push(() => {
+			(isAutonomous ? matchData.autonomous : matchData.teleop).coral.stationPickup--;
+			scoringState.hasCoral = false;
+		});
 	}
 	function coralGroundIntake() {
 		(isAutonomous ? matchData.autonomous : matchData.teleop).coral.groundIntake++;
 		scoringState.hasCoral = true;
+		actionUndos.push(() => {
+			(isAutonomous ? matchData.autonomous : matchData.teleop).coral.groundIntake--;
+			scoringState.hasCoral = false;
+		});
 	}
 
 	function coralL1() {
 		(isAutonomous ? matchData.autonomous : matchData.teleop).coral.l1++;
 		scoringState.hasCoral = false;
+		actionUndos.push(() => {
+			(isAutonomous ? matchData.autonomous : matchData.teleop).coral.l1--;
+			scoringState.hasCoral = true;
+		});
 	}
 	function coralL2() {
 		(isAutonomous ? matchData.autonomous : matchData.teleop).coral.l2++;
 		scoringState.hasCoral = false;
+		actionUndos.push(() => {
+			(isAutonomous ? matchData.autonomous : matchData.teleop).coral.l2--;
+			scoringState.hasCoral = true;
+		});
 	}
 	function coralL3() {
 		(isAutonomous ? matchData.autonomous : matchData.teleop).coral.l3++;
 		scoringState.hasCoral = false;
+		actionUndos.push(() => {
+			(isAutonomous ? matchData.autonomous : matchData.teleop).coral.l3--;
+			scoringState.hasCoral = true;
+		});
 	}
 	function coralL4() {
 		(isAutonomous ? matchData.autonomous : matchData.teleop).coral.l4++;
 		scoringState.hasCoral = false;
+		actionUndos.push(() => {
+			(isAutonomous ? matchData.autonomous : matchData.teleop).coral.l4--;
+			scoringState.hasCoral = true;
+		});
 	}
 	function coralDropped() {
 		(isAutonomous ? matchData.autonomous : matchData.teleop).coral.dropped++;
 		scoringState.hasCoral = false;
+		actionUndos.push(() => {
+			(isAutonomous ? matchData.autonomous : matchData.teleop).coral.dropped--;
+			scoringState.hasCoral = true;
+		});
 	}
 
 	function algaeReefDrop() {
 		(isAutonomous ? matchData.autonomous : matchData.teleop).algae.reefDescoreDrop++;
+		actionUndos.push(() => {
+			(isAutonomous ? matchData.autonomous : matchData.teleop).algae.reefDescoreDrop--;
+		});
 	}
 	function algaeReefTake() {
 		(isAutonomous ? matchData.autonomous : matchData.teleop).algae.reefDescoreTake++;
 		scoringState.hasAlgae = true;
+		actionUndos.push(() => {
+			(isAutonomous ? matchData.autonomous : matchData.teleop).algae.reefDescoreTake--;
+			scoringState.hasAlgae = false;
+		});
 	}
 	function algaeGroundIntake() {
 		(isAutonomous ? matchData.autonomous : matchData.teleop).algae.groundIntake++;
 		scoringState.hasAlgae = true;
+		actionUndos.push(() => {
+			(isAutonomous ? matchData.autonomous : matchData.teleop).algae.groundIntake--;
+			scoringState.hasAlgae = false;
+		});
 	}
 
 	function algaeProcessor() {
 		(isAutonomous ? matchData.autonomous : matchData.teleop).algae.processor++;
 		scoringState.hasAlgae = false;
+		actionUndos.push(() => {
+			(isAutonomous ? matchData.autonomous : matchData.teleop).algae.processor--;
+			scoringState.hasAlgae = true;
+		});
 	}
 	function algaeNet() {
 		(isAutonomous ? matchData.autonomous : matchData.teleop).algae.net++;
 		scoringState.hasAlgae = false;
+		actionUndos.push(() => {
+			(isAutonomous ? matchData.autonomous : matchData.teleop).algae.processor--;
+			scoringState.hasAlgae = true;
+		});
 	}
 	function algaeDropped() {
 		(isAutonomous ? matchData.autonomous : matchData.teleop).algae.dropped++;
 		scoringState.hasAlgae = false;
+		actionUndos.push(() => {
+			(isAutonomous ? matchData.autonomous : matchData.teleop).algae.dropped--;
+			scoringState.hasAlgae = true;
+		});
 	}
+
+	let coralScored = $derived(
+		matchData.autonomous.coral.l1 +
+			matchData.autonomous.coral.l2 +
+			matchData.autonomous.coral.l3 +
+			matchData.autonomous.coral.l4 +
+			matchData.teleop.coral.l1 +
+			matchData.teleop.coral.l2 +
+			matchData.teleop.coral.l3 +
+			matchData.teleop.coral.l4,
+	);
+	let coralTaken = $derived(
+		matchData.autonomous.coral.groundIntake +
+			matchData.autonomous.coral.stationPickup +
+			matchData.teleop.coral.groundIntake +
+			matchData.teleop.coral.stationPickup,
+	);
+	let algaeScored = $derived(
+		matchData.autonomous.algae.net +
+			matchData.autonomous.algae.processor +
+			matchData.teleop.algae.net +
+			matchData.teleop.algae.processor,
+	);
+	let algaeDescored = $derived(
+		matchData.autonomous.algae.reefDescoreDrop +
+			matchData.autonomous.algae.reefDescoreTake +
+			matchData.teleop.algae.reefDescoreDrop +
+			matchData.teleop.algae.reefDescoreTake,
+	);
+	let algaeTaken = $derived(
+		matchData.autonomous.algae.reefDescoreTake +
+			matchData.autonomous.algae.groundIntake +
+			matchData.teleop.algae.reefDescoreTake +
+			matchData.teleop.algae.groundIntake,
+	);
 </script>
 
-Coral Scored: {matchData.autonomous.coral.l1 +
-	matchData.autonomous.coral.l2 +
-	matchData.autonomous.coral.l3 +
-	matchData.autonomous.coral.l4 +
-	matchData.teleop.coral.l1 +
-	matchData.teleop.coral.l2 +
-	matchData.teleop.coral.l3 +
-	matchData.teleop.coral.l4}, Algae Scored: {matchData.autonomous.algae.net +
-	matchData.autonomous.algae.processor +
-	matchData.teleop.algae.net +
-	matchData.teleop.algae.processor}
+<p>Coral scored: {coralScored} | Algae scored: {algaeScored} | Algae descored: {algaeDescored}</p>
+<p>Coral taken: {coralTaken} | Algae taken: {algaeTaken}</p>
+<div>
+	<Button icon={undoIcon} disabled={undoCooldown || actionUndos.length == 0} onclick={undoLastAction}
+		>Undo last action</Button
+	>
+</div>
 <div class="scoring-grid full-height">
 	{#if !scoringState.hasCoral}
 		<div>
@@ -84,7 +188,7 @@ Coral Scored: {matchData.autonomous.coral.l1 +
 			<Button extraClasses="scoring-grid__button" onclick={coralGroundIntake}>Coral taken from ground</Button>
 		</div>
 		<div>
-			{#if isAutonomous && !matchData.autonomous.coral.preloaded}
+			{#if isAutonomous && !matchData.autonomous.coral.preloaded && coralTaken == 0}
 				<Button extraClasses="scoring-grid__button" onclick={coralPreload}>Coral preloaded</Button>
 			{/if}
 		</div>
